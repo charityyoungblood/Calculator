ead33b890301e46c1323e56bdd27d9089b9c6b7f
charityyoungblood <youngblood.charity@gmail.com>
2018-02-09 18:20:32 -0800

    updated keyword from var to let constant

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class CalculatorViewController: UIViewController {
 
     @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle
+        print("touched \(digit) digit")
    
     }
     
