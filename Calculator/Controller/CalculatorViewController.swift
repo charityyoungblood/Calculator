44eaededc390bc155a1fc472f1783b7225542a7c
charityyoungblood <youngblood.charity@gmail.com>
2018-02-12 16:27:29 -0800

    created Model, View, Controller folders; created CalculatorBrain file in Model folder

--- Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Controller/CalculatorViewController.swift
@@ -32,6 +32,21 @@ class CalculatorViewController: UIViewController {
             // i.e. WITH this line, the display reads 76854
             // WITHOUT this line, the display reads 4, then deletes the 4 to show 5, then deletes the 5 to show 6 -- i.e. ONLY shows ONE NUMBER at a time 
     }
+    // *** REMEMEBER: When creating the methods for your code, think about how to simplify, always making your code D.R.Y
+    // In the case of type converting every operation to a String, this would not be effective as you would have to change it back to a Double to operate on the value
+    // Since the value that is displayed on the calculator will ALWAYS be a Double, we can create a variable that automatically tracked what was in the display
+    // Instead of creating a "storing" variable, we will create a "calculated" variable -- also known as a "Computed Property"
+    // Instead of using = to set the variable, we place curly braces after the variable name and data type
+    // Inside the curly brackets, place code to calculate the value when we "get" the value and when we "set" the value
+    
+    var displayValue: Double { // this is an example of a "Computed Property/Variable"
+        get {
+           return Double(display.text!)! // when we "get" or request the value of displayValue, we want to receieve(return) the display.text with the Double data type
+        }
+        set {
+           display.text = String(newValue) // when we "set" the value of displayValue, we want to set it to newValue with the String data type 
+        }
+    }
     
     @IBAction func piOperation(_ sender: UIButton) {
         userIsInTheMiddleOfTyping = false
@@ -40,11 +55,10 @@ class CalculatorViewController: UIViewController {
         // you want to force unwrap only if UIButton is "set", meaning it's not "nil"
             if mathematicalSymbol == "π" {
                 display.text = String(Double.pi) // to convert the type for M_PI, which is Double, to String type - use String keyword and wrap value (M_PI) in parentheses
-            }
         }
         
     }
-
+    }
    
 // ** How do I change corner radius of all buttons?
     
@@ -60,3 +74,4 @@ class CalculatorViewController: UIViewController {
 
 
 }
+
---
+++ Calculator/Model/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Charity Youngblood  on 2/12/18.
+//  Copyright © 2018 Charity Youngblood . All rights reserved.
+//
+
+import Foundation
