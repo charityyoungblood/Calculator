2ab1f3f2baf390454d265f4b140c71d977ce313e
charityyoungblood <youngblood.charity@gmail.com>
2018-02-12 17:15:32 -0800

    created setOperand, performOperation and results (computed property/variable) in CalculatorBrain class

--- Calculator/Model/CalculatorBrain.swift
+++ Calculator/Model/CalculatorBrain.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2018 Charity Youngblood . All rights reserved.
 //
 
-import Foundation
+import Foundation // as the Model is UI Independent DO NOT import UI Kit in the Model 
+
+// When building a Model - think about the properties your Model should have
+    // In the case of the calculator - operation type?
+    // This class should hold all of the functions for operating the calculator
+
+class CalculatorBrain {
+    
+    func setOperand(operand: Double) {
+        
+    }
+    
+    func performOperation() { // this function will operate on the operand
+        // the symbol parameter is the String data type of the mathematical symbol 
+        
+    }
+    
+    var
+}
